df84c8efcf1c804438b8919487d10c0a4bad65a7
Sakhawat Hossain <sakhawath847@gmail.com>
2025-12-20 19:53:39 +0600

    Add responsive auth UI for login and signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import './Login.css';
+import { PiHandsClappingFill } from "react-icons/pi";
+
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className="auth-page">
+            <div className="auth-card">
+
+                {/* Left Side */}
+                <div className="auth-left">
+                    <h1 className="auth-title">
+                        Welcome back <PiHandsClappingFill />
+                    </h1>
+                    <p className="auth-subtitle">
+                        Login to continue your journey
+                    </p>
+                </div>
+
+                {/* Right Side */}
+                <div className="auth-right">
+                    <h1 className="auth-right-title">Login</h1>
+                    <form className="login-form">
+                        <input
+                            type="text"
+                            className="input-field"
+                            name="emailOrphone"
+                            placeholder=' Email or phone number...'
+                            required
+                        />
+
+                        <input
+                            type="password"
+                            className="input-field"
+                            name='password'
+                            placeholder='Password..'
+                            required
+                        />
+
+                        <button className="button">Login</button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div >
+
     )
 }
 
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
+import '../Login/Login.css';
 
 const Signup = () => {
     return (
-        <div>Signup</div>
+        <div className="auth-page">
+            <div className="auth-card">
+
+                {/* Left */}
+                <div className="auth-left">
+                    <h1 className="auth-title">Create Account 🎉</h1>
+                    <p className="auth-subtitle">
+                        Sign up to start your journey
+                    </p>
+                </div>
+
+                {/* Right */}
+                <div className="auth-right">
+                    <h1 className="auth-right-title">Sign Up</h1>
+
+                    <form className="login-form">
+
+                        <input
+                            type="text"
+                            name="username"
+                            className="input-field"
+                            placeholder="Username"
+                            required
+                        />
+
+                        <input
+                            type="email"
+                            name="email"
+                            className="input-field"
+                            placeholder="Email address"
+                            required
+                        />
+
+                        <input
+                            type="text"
+                            name="phone_number"
+                            className="input-field"
+                            placeholder="Phone number (optional)"
+                        />
+
+                        <input
+                            type="password"
+                            name="password"
+                            className="input-field"
+                            placeholder="Password"
+                            required
+                        />
+
+
+                        <input
+                            type="password"
+                            className="input-field"
+                            placeholder="Confirm password"
+                            required
+                        />
+
+                        <button className="button">Create Account</button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
